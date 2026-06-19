4b3665088dd87f8c3c2d417fbad4b800642f9ec0
momotam103-ui <momotam103@gmail.com>
2026-06-19 12:32:39 +0900

    tuiy

--- app.js
+++ app.js
@@ -9,18 +9,51 @@ let connects = []
 
 app.use(express.static('public'))
 
+function broadcast(data) {
+  const message = JSON.stringify(data)
+
+  connects.forEach((socket) => {
+    if (socket.readyState === 1) {
+      socket.send(message)
+    }
+  })
+}
+
 app.ws('/ws', (ws, req) => {
   connects.push(ws)
 
   ws.on('message', (message) => {
     console.log('Received:', message)
 
-    connects.forEach((socket) => {
-      if (socket.readyState === 1) {
-        // Check if the connection is open
-        socket.send(message)
+    try {
+      const data = JSON.parse(message)
+
+      if (data.type === 'chat') {
+        broadcast({
+          type: 'chat',
+          id: data.id,
+          name: data.name,
+          text: data.text,
+          color: data.color,
+          time: new Date().toLocaleTimeString('ja-JP', {
+            hour: '2-digit',
+            minute: '2-digit'
+          })
+        })
       }
-    })
+
+      if (data.type === 'join') {
+        broadcast({
+          type: 'system',
+          text: `${data.name} さんが入室しました。`
+        })
+      }
+    } catch (e) {
+      broadcast({
+        type: 'system',
+        text: 'メッセージの形式が正しくありません。'
+      })
+    }
   })
 
   ws.on('close', () => {
